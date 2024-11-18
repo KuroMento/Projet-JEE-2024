43cdd534f73bd5af4ab3de0ed8350e3582ae0921
Matthias1001 <163323963+Matthias1001@users.noreply.github.com>
2024-11-18 18:01:00 +0100

    Update FrontController.java

--- Hibernate/src/main/java/fr/cyu/jee/controller/FrontController.java
+++ Hibernate/src/main/java/fr/cyu/jee/controller/FrontController.java
@@ -11,8 +11,23 @@ public class FrontController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String action = req.getParameter("action");
+        String categorie = req.getParameter("categorie");
         if (action != null && action.equals("login")){
             req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
+        }
+        if(categorie != null) {
+            if (categorie.equals("subject")) {
+                req.getRequestDispatcher("/WEB-INF/subject.jsp").forward(req, resp);
+            }
+            if (categorie.equals("class")) {
+                req.getRequestDispatcher("/WEB-INF/class.jsp").forward(req, resp);
+            }
+            if (categorie.equals("user")) {
+                req.getRequestDispatcher("/WEB-INF/user.jsp").forward(req, resp);
+            }
+            if (categorie.equals("grade")) {
+                req.getRequestDispatcher("/WEB-INF/grade.jsp").forward(req, resp);
+            }
         }
         else {
             req.getRequestDispatcher("/index.jsp").forward(req, resp);
