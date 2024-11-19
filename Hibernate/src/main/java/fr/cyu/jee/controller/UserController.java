17415c187c16a982da2e65aa82410a00ad9573b2
Matthias1001 <163323963+Matthias1001@users.noreply.github.com>
2024-11-19 15:35:48 +0100

    Added UserController
    UserController returns a list of all the users in the database to show them when needed.

---
+++ Hibernate/src/main/java/fr/cyu/jee/controller/UserController.java
@@ -0,0 +1,40 @@
+package fr.cyu.jee.controller;
+
+import fr.cyu.jee.HibernateUtil;
+import fr.cyu.jee.model.User;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import org.hibernate.Session;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Servlet that returns the list of users.
+ */
+public class UserController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<User> userList = getListUsers();
+        req.setAttribute("UserList",userList);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    /**
+     *
+     * @return The users in the database as a list.
+     */
+    public static List<User> getListUsers() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        List<User> result = session.createQuery("from User").list();
+        session.close();
+        return result;
+    }
+
+}
