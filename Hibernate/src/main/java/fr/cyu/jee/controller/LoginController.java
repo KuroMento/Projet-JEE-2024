dcb8a0136ef7bc00c1dd461020ce4a26ff5e9aca
Matthias1001 <163323963+Matthias1001@users.noreply.github.com>
2024-11-19 15:22:44 +0100

    Update LoginController.java

--- Hibernate/src/main/java/fr/cyu/jee/controller/LoginController.java
+++ Hibernate/src/main/java/fr/cyu/jee/controller/LoginController.java
@@ -7,11 +7,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 import org.hibernate.Session;
 
@@ -21,22 +18,14 @@ import org.hibernate.Session;
 public class LoginController extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String lastName = req.getParameter("lastName");
-        String firstName = req.getParameter("firstName");
-        String contact = req.getParameter("contact");
+        String lastName = null;
+        String firstName = null;
+        String contact = null;
         String identification = req.getParameter("identification");
         String cryptedPassword = req.getParameter("cryptedPassword");
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd", Locale.FRENCH);
-        String dateOfBirthString = req.getParameter("dateOfBirth");
         Date dateOfBirth = null;
         Permissions permissions = null;
-        try {
-            dateOfBirth = formatter.parse(dateOfBirthString); //Caution : there might be errors if the incorrect date format is entered
-        }
-        catch (ParseException e){
-            req.setAttribute("dateError","The date format is not correct! (it should be 'YYYY/MM/DD')");
-        }
-        if(lastName.isEmpty() || firstName.isEmpty() || contact.isEmpty() || identification.isEmpty() || cryptedPassword.isEmpty() || dateOfBirth == null){ //if the data entered is not valid
+        if(identification.isEmpty() || cryptedPassword.isEmpty()){ //if the data entered is not valid
             req.setAttribute("error", "All data must be completed"); //pas la bonne méthode, à changer mais reste la pour l'instant
             req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
         }
@@ -54,13 +43,13 @@ public class LoginController extends HttpServlet{
     }
 
     /**
-     * 
+     *
      * @return The users in the database as a list.
      */
     public static List<User> getListUsers() {
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
-        List<User> result = session.createQuery("from Users").list();
+        List<User> result = session.createQuery("from User").list();
         session.close();
         return result;
     }
@@ -76,7 +65,7 @@ public class LoginController extends HttpServlet{
         List<User> userList;
         userList = getListUsers();
         for (int i = 0; i < userList.size(); i++) {
-            if (user.getFirstName().equals(userList.get(i).getFirstName()) && user.getLastName().equals(userList.get(i).getLastName()) && user.getContact().equals(userList.get(i).getContact()) && user.getIdentification().equals(userList.get(i).getIdentification()) && user.getCryptedPassword().equals(userList.get(i).getCryptedPassword()) && user.getDateOfBirth().equals(userList.get(i).getDateOfBirth())) {//if a user is in the database, then return his id in the base and confirm the login
+            if (user.getIdentification().equals(userList.get(i).getIdentification()) && user.getCryptedPassword().equals(userList.get(i).getCryptedPassword())) {//if a user is in the database, then return his id in the base and confirm the login
                 givePermissions(user, i);
                 session.getTransaction().commit();
                 session.close();
