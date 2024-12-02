28662e61d5d46abf549174eb2a3437c827a10cc4
NoliroSlime <oliviercminarro@gmail.com>
2024-12-02 19:51:26 +0100

    Updated GradeController to add grade report
    Added mean calculation for the grade report and deleted unneeded files

--- SpringBoot/src/main/java/fr/cyu/jee/controller/GradeController.java
+++ SpringBoot/src/main/java/fr/cyu/jee/controller/GradeController.java
@@ -1,6 +1,8 @@
 package fr.cyu.jee.controller;
 
 import fr.cyu.jee.ModelValidator;
+import fr.cyu.jee.model.Course;
+import fr.cyu.jee.model.Grade;
 import fr.cyu.jee.model.Permissions;
 import fr.cyu.jee.model.User;
 import fr.cyu.jee.repository.GradeRepository;
@@ -9,13 +11,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 public class GradeController {
     @Autowired
     private GradeRepository gradeRepository;
     @GetMapping("/grade")
-    public String gradePage(HttpSession session, Model model){
+    public String gradePage(HttpSession session, Model model, @RequestParam(defaultValue = "") String option){
         User loggedUser = (User) session.getAttribute("loggedUser");
         try {
             if (loggedUser != null) {
@@ -28,6 +34,17 @@ public class GradeController {
                 }
                 if (loggedUser.getPermissions() == Permissions.STUDENT) {
                     ModelValidator.validateParameter(loggedUser.getIdentification());
+                    //Operations for student
+                    if(option.equals("report")){
+                        model.addAttribute("option", "report");
+                        model.addAttribute("selectedGrade", new Grade());
+                        List<Grade> gradeList = gradeRepository.findAllByStudent_Identification(loggedUser.getIdentification());
+                        List<Course> courseList = new LinkedList<>();
+                        for(Grade grade : gradeList){
+                            courseList.add(grade.getCourse());
+                        }
+                        model.addAttribute("courses", courseList);
+                    }
                     model.addAttribute("grades", gradeRepository.findAllByStudent_Identification(loggedUser.getIdentification()));
                 }
             }
